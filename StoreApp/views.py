3302602f8d779c58ea76bd1c7d97065d102b3a31
Guilherme Lima <senacguilherme13@gmail.com>
2023-12-04 20:07:34 -0300

    Mudanças

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
-from StoreApp.models import Departamento
+from StoreApp.models import Departamento, Produto
 
 # Create your views here.
 def index(request):
-   return render(request, 'index.html')
-
-
-
+  produtos_destaque = Produto.objects.filter(destaque = True)
+   
+  context = {
+       'produtos' : produtos_destaque
+     }
+  
+  return render(request, 'index.html', context)
 
 def produto_lista(request):
-    return render(request, 'produtos.html')
+    produtos = Produto.objects.all()
 
-def produto_detalhe(request):
-    return render(request, 'produto_detalhes.html')
+    context = {
+        'produtos' : produtos,
+        'categorias' : 'Todos os Produtos'
+    }
+    
+    return render(request, 'produtos.html', context)
+
+def produto_lista_por_id(request, id):
+    produtos = Produto.objects.filter(departamento_id = id)
+    departamento = Departamento.objects.get(id = id)
+    
+    context = {
+        'produtos': produtos,
+        'categoria': departamento.nome,
+    }
+    
+    return render(request, 'produtos.html', context)
+
+
+def produto_detalhe(request, id):
+    produto = Produto.objects.get(id = id)
+    produtos_relacionados = Produto.objects.filter(departamento_id = produto.departamento.id).exclude(id = id)[:4]
+
+    context = {
+    'produto' : produto,
+    'produtos_relacionados' : produtos_relacionados
+    
+    }
+    
+    return render(request, 'produto_detalhes.html', context)
